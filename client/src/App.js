5a4612e99138978aa1e16b8544b98cea6052a80d
Kevin Gao <kevinmingjiang@gmail.com>
2022-03-27 09:13:14 -0400

    always redirect if login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import store from "./redux/store";
 
@@ -15,38 +15,68 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route exact path="/" element={<LandingPage />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<SignUp />} />
-            <Route
-              exact
-              path="/feed"
-              element={
-                <Fragment>
-                  <Navbar />
-                  <Feed />
-                </Fragment>
-              }
-            />
-            <Route
-              path="/question/:questionID"
-              element={
-                <Fragment>
-                  <Navbar />
-                  <Question />
-                </Fragment>
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Router>
+				<div className="App">
+					<Routes>
+						<Route
+							exact
+							path="/"
+							element={
+								localStorage.getItem("accessToken") === null ? (
+									<LandingPage />
+								) : (
+									<Navigate replace to="/feed" />
+								)
+							}
+						/>
+						<Route
+							exact
+							path="/login"
+							element={
+								localStorage.getItem("accessToken") === null ? (
+									<Login />
+								) : (
+									<Navigate replace to="/feed" />
+								)
+							}
+						/>
+						<Route
+							exact
+							path="/register"
+							element={
+								localStorage.getItem("accessToken") === null ? (
+									<SignUp />
+								) : (
+									<Navigate replace to="/feed" />
+								)
+							}
+						/>
+						<Route
+							exact
+							path="/feed"
+							element={
+								<Fragment>
+									<Navbar />
+									<Feed />
+								</Fragment>
+							}
+						/>
+						<Route
+							path="/question/:questionID"
+							element={
+								<Fragment>
+									<Navbar />
+									<Question />
+								</Fragment>
+							}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</Provider>
+	);
 }
 
 export default App;
